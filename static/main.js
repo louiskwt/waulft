c879644f11f04681a6f81855c4f82ed8e8ebb90b
Louis Tsang <kawingt2@gmail.com>
2024-02-07 14:35:02 +0800

    handle mobile select

--- static/main.js
+++ static/main.js
@@ -48,6 +48,51 @@ function handleUnitChange(btn, value, currentUnit) {
   });
 }
 
+function handleUnitSelect(currentUnit, target, value) {
+  switch (true) {
+    case target === "days":
+      if (["day", "days"].includes(currentUnit.textContent) === false) {
+        const newValue = localStorage.getItem("time");
+        value.textContent = newValue;
+        currentUnit.textContent = newValue > 1 ? "days" : "day";
+      }
+      break;
+    case target === "hours":
+      if (["hour", "hours"].includes(currentUnit.textContent) === false) {
+        const newValue = localStorage.getItem("time") * 24;
+        value.textContent = newValue;
+        currentUnit.textContent = newValue > 1 ? "hours" : "hour";
+      }
+      break;
+    case target === "minutes":
+      console.log(currentUnit.textContent, "here");
+      if (["minute", "minutes"].includes(currentUnit.textContent) === false) {
+        const newValue = localStorage.getItem("time") * 24 * 60;
+        value.textContent = newValue;
+        currentUnit.textContent = newValue > 1 ? "minutes" : "minute";
+      }
+      break;
+    case target === "seconds":
+      if (["second", "seconds"].includes(currentUnit.textContent) === false) {
+        const newValue = localStorage.getItem("time") * 24 * 60 ** 2;
+        value.textContent = newValue;
+        currentUnit.textContent = newValue > 1 ? "seconds" : "second";
+      }
+      break;
+    case target === "milliseconds":
+      if (currentUnit.textContent !== "millisecond") {
+        const newValue = localStorage.getItem("time") * 24 * 60 ** 2 * 1000;
+        value.textContent = newValue;
+        currentUnit.textContent = newValue > 1 ? "milliseconds" : "millisecond";
+      }
+      break;
+    default:
+      value.textContent = localStorage.getItem("time");
+      currentUnit.textContent = localStorage.getItem("time") > 1 ? "days" : "day";
+      break;
+  }
+}
+
 window.onload = function () {
   const btnDay = document.getElementById("btn-d");
   const btnHour = document.getElementById("btn-h");
@@ -65,4 +110,10 @@ window.onload = function () {
   handleUnitChange(btnMinute, time, unit);
   handleUnitChange(btnSecond, time, unit);
   handleUnitChange(btnMillisecond, time, unit);
+
+  // handle mobile select
+  const select = document.getElementById("menu-dropdown");
+  select.addEventListener("change", (e) => {
+    handleUnitSelect(unit, e.target.value, time);
+  });
 };

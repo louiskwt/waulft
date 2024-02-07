9513c5bc92c177171440bb3f8286a7107ffa68c2
Louis Tsang <kawingt2@gmail.com>
2024-02-07 08:18:52 +0800

    calculated and passed time to template

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    today = datetime.date.today()
+    until = datetime.date(2024, 2, 9)
+    diff = (until - today).days
+    return render_template('index.html', time_unit=diff)
